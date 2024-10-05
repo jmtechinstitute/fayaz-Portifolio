ea6ee4fe3b1648d44516a908637231d380d55248
Mohamed Yazeen <166999856+jmtechinstitute@users.noreply.github.com>
2024-10-05 21:25:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings : ["JM TECH CEO","Mentor at Jm Tech","Full Stack Developer","Python Pogrammer","c & c++ Programmer","B.Com Student"],
+    strings : ["JM TECH CEO","Mentor at Jm Tech","Full Stack Developer","Python Pogrammer","c & c++ Programmer","B.Tech Student"],
     typeSpeed:30,
     backSpeed:50,
     backdelay:1000,
